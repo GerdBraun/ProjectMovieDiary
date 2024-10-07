3c78268a1ea4cdd41f04e57c52fef66fd1024c87
Gerd Braun <webmaster@your-d-sign.de>
2024-10-08 00:14:20 +0200

    minimal preview added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ class Movie {
     // #id = 0;    // the id of the movie ! we'll take it from the data object
     #data = {};  // for storing the fetched data object
     #details = {} // for storing the fetched detail object
+    #mainInstance = {}; // the main instance for event handlers
 
     //constructor method
     constructor(movieObject) {
@@ -14,6 +15,10 @@ class Movie {
     }
 
     // getters & setters
+    set mainInstance(val) {
+        this.#mainInstance = val
+    }
+
     set id(val) {
         this.#data.id = val
     }
@@ -34,7 +39,7 @@ class Movie {
         return this.#details;
     }
 
-    fetchMovieDetails(){
+    fetchMovieDetails() {
         // TODO: fetch the detail data
         // url-example: 'https://api.themoviedb.org/3/movie/533535?language=en-US&api_key=153a09fbeef547fb0435feeeb75d0140'
         return details;
@@ -44,13 +49,27 @@ class Movie {
      * renders the output
      * @returns {Element} the element to be displayed
      */
-    renderView(){
+    renderView(pathToImages) {
         // TODO: fetch detail data first!
 
         // TODO: build output...
+        const out = document.createElement('div');
+
         const test = document.createElement('p');
-        test.textContent = this.data.original_title,' (created by Movie.renderView)';
-        return test;
+        test.innerHTML = '<strong>"' + this.data.title+'"</strong> ['+this.data.release_date+']';
+        if (this.data.title !== this.data.original_title) test.innerHTML += '<br>(' + this.data.original_title + ')';
+        test.innerHTML += '<br>created by Movie.renderView<br>has to be fetched every time';
+        out.appendChild(test);
+
+        const img = document.createElement('img');
+        img.src = pathToImages + this.data.poster_path;
+        out.appendChild(img);
+
+        const overview = document.createElement('p');
+        overview.textContent = this.data.overview;
+        out.appendChild(overview);
+
+        return out;
     }
 }
 
@@ -60,12 +79,17 @@ class Movie {
 class MovieList {
     // private vars
     #list = []; // an array for Movies
+    #mainInstance = {}; // the main instance for event handlers
 
     constructor() {
         // do whatever needed
     }
 
     // getters & setters
+    set mainInstance(val) {
+        this.#mainInstance = val
+    }
+
     set list(val) {
         this.#list = val
     }
@@ -78,7 +102,7 @@ class MovieList {
      * @param {Movie} movie 
      */
     addMovie(movie) {
-        console.log(`movie "${movie.data.original_title}" was added to the MovieList`);
+        console.log(`movie "${movie.data.title}" was added to the MovieList`);
         this.list.push(movie);
     }
 
@@ -90,7 +114,7 @@ class MovieList {
     getMovieById(id) {
         // return the first element of the filtered results
         return this.#list.filter((movie) => {
-            return movie.id === id
+            return movie.id === parseInt(id)
         })[0]
     }
 
@@ -104,21 +128,27 @@ class MovieList {
         this.list.forEach((movie) => {
             const li = document.createElement('li');
             li.classList = 'flex p-2 pl-3 text-gray-800 bg-white rounded shadow mb-1 justify-between items-center'
-            li.textContent = movie.data.original_title;
+            li.textContent = movie.data.title;
 
             const span = document.createElement('span');
             span.classList = 'flex gap-2';
 
             const viewBtn = document.createElement('button');
-            viewBtn.classList = 'movie-button movie-button-green';
+            viewBtn.classList = 'action-button movie-button movie-button-green';
             viewBtn.textContent = 'view';
             viewBtn.dataset.id = movie.data.id;
+            viewBtn.dataset.action = 'view';
+            viewBtn.dataset.caller = this.constructor.name; // pass the name of the Class
+            viewBtn.addEventListener('click', (event) => this.#mainInstance.eventHandler(event));
             span.appendChild(viewBtn);
 
             const addBtn = document.createElement('button');
-            addBtn.classList = 'movie-button movie-button-green';
+            addBtn.classList = 'action-button movie-button movie-button-green';
             addBtn.textContent = 'add';
             addBtn.dataset.id = movie.data.id;
+            addBtn.dataset.action = 'add';
+            addBtn.dataset.caller = this.constructor.name; // pass the name of the Class
+            addBtn.addEventListener('click', (event) => this.#mainInstance.eventHandler(event));
             span.appendChild(addBtn);
 
             li.appendChild(span);
@@ -139,6 +169,8 @@ class MovieFavoritesList extends MovieList {
     constructor() {
         // initiate the super class (MovieList), so this has everything from there
         super();
+
+        this.getListFromLocalStorage();
     }
 
     // getters & setters
@@ -187,6 +219,9 @@ class Main {
     constructor() {
         // do important things
 
+        // pass the main instance for event listeners
+        this.#movieList.mainInstance = this;
+        this.#movieFavoritesList.mainInstance = this;
     }
 
     // getters & setters
@@ -268,15 +303,23 @@ class Main {
         arr.forEach(movieObject => {
             // create new instance of Movie (and pass the data object)
             const movie = new Movie(movieObject);
+            movie.mainInstance = this;
             // add the movie instance to the list
             this.#movieList.addMovie(movie);
         });
         //this.#movieList.renderView();
 
-        this.renderView('movieList');
+        this.renderView('movieList', 1745);
     }
 
-    renderView(output, ...data) {
+    /**
+     * renders the output
+     * @param {String} output the target to put the content to
+     * @param {Object} sourceList the source list of the data (MovieList or MovieFavList)
+     * @param {*} movieId the ID of the movie to display
+     */
+    renderView(output, sourceList = null, movieId = null) {
+
         let outContainer = null;
         let content = null;
 
@@ -284,17 +327,53 @@ class Main {
             case 'movieList':
                 // render the movieList
                 outContainer = document.querySelector(this.#movieListView);
+                outContainer.innerHTML = '';
                 content = this.#movieList.renderView();
                 outContainer.appendChild(content);
                 break;
-            case 'movieFavoriteList':
+            case 'movieFavoritesList':
                 // render the movieFavoriteList
-                outContainer = document.querySelector(this.#movieListView);
-                content = this.#movieList.renderView();
+                outContainer = document.querySelector(this.#movieFavoritesListView);
+                outContainer.innerHTML = '';
+                content = this.#movieFavoritesList.renderView();
                 outContainer.appendChild(content);
+                break;
+            case 'details':
+                // render the details
+                outContainer = document.querySelector(this.#detailView);
+                outContainer.innerHTML = '';
+                let movie;
+                if (sourceList === 'MovieList') {
+                    movie = this.#movieList.getMovieById(movieId)
+                } else {
+                    movie = this.#movieFavoritesList.getMovieById(movieId)
+                }
+                content = movie.renderView(this.pathToImages);
+                outContainer.appendChild(content);
+        }
+    }
+
+    /**
+     * handle the events sent to main instance
+     * @param {Event} event 
+     */
+    eventHandler(event) {
+        console.log(event.target.dataset);
+
+        const dataset = event.target.dataset;
+
+        switch (dataset.action) {
+            case 'view':
+                // show detail view
+                this.renderView('details', dataset.caller, dataset.id);
+                break;
+            case 'add':
+                // add movie to favorites
+                break;
+            case 'remove':
+                // remove movies from favorites
                 break;
             default:
-            // render the details
         }
     }
 }
@@ -305,14 +384,15 @@ const mainInstance = new Main();
 // set initial values
 mainInstance.apiKey = '153a09fbeef547fb0435feeeb75d0140' // use it as url-parameter like 'api_key=153a09fbeef547fb0435feeeb75d0140'
 mainInstance.pathToTmdb = 'https://api.themoviedb.org/3/movie'; // the main path to the movies
-mainInstance.pathToImages = 'https://image.tmdb.org/t/p/original'; // see readdme.md for other options
+// mainInstance.pathToImages = 'https://image.tmdb.org/t/p/original'; // see readdme.md for other options
+mainInstance.pathToImages = 'https://image.tmdb.org/t/p/w780'; // see readdme.md for other options
 mainInstance.initialCall = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'; // the url params to call first
 mainInstance.localStorageName = 'movieFavs';
 
 // output-container for views
 mainInstance.movieListView = '#movieList';
 mainInstance.movieFavoritesListView = '#moviefavoritesList';
-mainInstance.detailView = '#detailView';
+mainInstance.detailView = '#movieDetails';
 
 // place the initial call 
 mainInstance.fetchInitial();
